fdb92248e196c089ceba8f7ca346388b13195b3b
ksando3 <kevinsandoval1727@gmail.com>
2022-07-24 16:01:19 -0700

    updated files

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 # 
-FROM python:3.9
+FROM python:latest
 
 # 
 WORKDIR /code
 
 # 
 COPY ./requirements.txt /code/requirements.txt
+
+#
+RUN pip install -U pip
 
 # 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
-from predict import predict, load_model
+from .predict import predict, load_model
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/get-prediction")
 def get_prediction(Pclass, SibSp, Parch, Sex_female, Sex_male):
-    print("Hello")
-    model = load_model()
+    model = load_model("./app/model.pkl")
     data = {"Pclass": [Pclass], "SibSp": [SibSp], "Parch": [Parch], "Sex_female": [Sex_female], "Sex_male": [Sex_male]}
     prediction = int(predict(model, data))
     if prediction == 1:
         return {"Prediction": "Survived"}
     else:
-        return {"Prediction": "rip bozo"}   
+        return {"Prediction": "Died"}   
     
